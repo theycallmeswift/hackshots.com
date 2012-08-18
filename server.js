d67366384b37f3747288e6f3516cbbfddcb3c82e
Mike Swift <theycallmeswift@gmail.com>
2012-08-18 16:15:43 -0400

    Heroku compliant

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
   , http = require('http')
-  , app = express();
+  , app = express()
+  , port = process.env.PORT || 3000;
 
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
