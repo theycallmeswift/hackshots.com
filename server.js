1683a90e68e9b0f8b13718dadb4ab0fa373eb4f8
Mike Swift <theycallmeswift@gmail.com>
2012-08-18 17:32:14 -0400

    Cluster and worker.

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-var express = require('express')
-  , http = require('http')
-  , querystring = require('querystring')
-  , util = require('util')
-  , app = express()
-  , request = require('request')
-  , port = process.env.PORT || 3000
-  , env = process.env.NODE_ENV || 'development';
+var cluster = require('cluster')
+  , clusterMaster = require("cluster-master")('worker.js');
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
-
-function forcewww(req, res, next) {
-  if(env === 'production') {
-    if (req.headers.host.match(/^www/) !== null ) {
-      next();
-    } else {
-      res.redirect('https://www.' + req.headers.host + req.url);
-    }
-  } else {
-    next();
-  }
-}
-
-app.get('/api/v1/i', [forcewww], function(req, res) {
-  var qs = querystring.stringify(req.query)
-    , api_url = "http://pinkyurl.com/i?" + qs;
-  util.log("Requesting: " + api_url);
-  request.get(api_url).pipe(res);
-});
-
-http.createServer(app).listen(port);
